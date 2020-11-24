9930ed1dc3dc9dbb9969bb7fbe16c9f2e90c7b15
luiizsilverio <luiiz.silverio@gmail.com>
2020-11-24 20:53:46 -0300

    envio do desafio 5

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,16 @@
 import Transaction from '../models/Transaction';
+// import CreateTransactionService from '../services/CreateTransactionService';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -13,16 +20,38 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
+  /*
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
+  }
+  */
+
+  public all(): { transactions: Transaction[]; balance: Balance } {
+    return { transactions: this.transactions, balance: this.getBalance() };
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance: Balance = { income: 0, outcome: 0, total: 0 };
+
+    this.transactions.forEach(transaction => {
+      balance[transaction.type] += transaction.value;
+      if (transaction.type === 'income') {
+        balance.total += transaction.value;
+      } else if (transaction.type === 'outcome') {
+        balance.total -= transaction.value;
+      }
+
+      return balance;
+    });
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
